337abd8645cea5158694ad7d7e90af67e676c21a
Aleh Biruk <aleh.biruk@odoscope.com>
2016-05-18 10:19:34 +0200

    notify as stat

--- main/run.go
+++ main/run.go
@@ -14,13 +14,14 @@ import (
 var addr = flag.String("l",":8881","addr")
 func main() {
 
-        log.Printf("Start Listening v2 %s",*addr)
-
         handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
                 log.Println(os.Getgid())
                 fmt.Fprintf(w, "Hello, %s\n", html.EscapeString(r.URL.Path))
         })
 
 
-        restartable.ListenAndServe(*addr,handler)
+        err := restartable.ListenAndServe(*addr,handler)
+        if err != nil {
+                panic(err)
+        }
 }
